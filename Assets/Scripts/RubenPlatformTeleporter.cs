04f7073cc7311d8ff4c96c3f7e479cf67acfd626
Ruben Sanchez <Ruben.SanchezVelasco@student.csulb.edu>
2017-02-10 13:33:47 -0800

    I created an infinite runner

---
+++ Assets/Scripts/RubenPlatformTeleporter.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RubenPlatformTeleporter : MonoBehaviour
+{
+    public int followDistance;
+    public Transform playerTransform;
+    public GameObject[] platformPositions;
+
+    private Vector3 _newPos;
+    private int _rand;
+
+	// Use this for initialization
+	void Start ()
+    {
+        playerTransform = GameObject.Find("Player").GetComponent<Transform>();
+        platformPositions = GameObject.FindGameObjectsWithTag("PlatformPosition");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        _newPos = new Vector3(playerTransform.position.x - followDistance, transform.position.y, 0);
+        transform.position = _newPos;
+
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Platform")
+        {
+            Debug.Log("Is a platform");
+            _rand = Random.Range(0, platformPositions.Length);
+            _newPos = platformPositions[_rand].transform.position;
+            other.transform.position = _newPos;
+
+            for(int i = 0; i < platformPositions.Length; i++)
+            {
+                platformPositions[i].transform.position = new Vector3(platformPositions[i].transform.position.x + 1.5f, platformPositions[i].transform.position.y, 0);
+            }
+
+        }
+    }
+}
---
+++ Assets/Scripts/RubenPlayerScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RubenPlayerScript : MonoBehaviour
+{
+    public Rigidbody2D rb2d;
+    public int jumpStrength;
+    
+
+	// Use this for initialization
+	void Start ()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        rb2d.AddForce(Vector2.right);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb2d.AddForce(Vector2.up * jumpStrength);
+        }
+	}
+}
