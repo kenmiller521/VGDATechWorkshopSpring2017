3105611e605cdaf0901a934e3ea31d29f00b1ab2
Ken Miller <ken.miller521@gmail.com>
2017-02-10 13:33:54 -0800

    I created an infinite runner!

---
+++ Assets/Scripts/KenCameraFollowScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KenCameraFollowScript : MonoBehaviour {
+	public Transform playerTransform;
+	private Vector3 _newPos;
+	// Use this for initialization
+	void Start () {
+		playerTransform = GameObject.Find ("Player").GetComponent<Transform> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		_newPos = new Vector3 (playerTransform.position.x, playerTransform.position.y, -10);
+		transform.position = _newPos;
+	}
+}
